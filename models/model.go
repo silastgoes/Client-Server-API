5894b701fff15357198b84cf0eeeb8ecba02c79e
silastgoes <silastgoes@gmail.com>
2023-12-03 17:30:12 -0300

    Criação de modelos para leitura das APIS

---
+++ models/model.go
@@ -0,0 +1,16 @@
+package models
+
+type Bid struct {
+	Id         int    `gorm:"primaryKey"`
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+}
